dc898a0c966ad50e8514711811f1a6c29c67f9b1
rostic54 <rostic54@gmail.com>
2017-07-15 18:17:38 +0300

    markup end

--- js/script.js
+++ js/script.js
@@ -1,12 +1,40 @@
 "use strict";
 
+function initMap() {
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 8.9194172, lng: 76.6418569},
+        scrollwheel: false,
+        zoom: 14
+    });
+}
+
+function submitForm() {
+    $( '#check' ).click( function () {
+         $( '.js-script-form' ).submit();
+    });
+}
+
 function bigSlider() {
     $( '.js-script-slider' ).slick( {
         infinite:true,
-        arrows: true
+        arrows: true,
+        //fade: true
 
     } );
 
+}
+
+function carousel() {
+    $( '.js-script-carousel' ).slick( {
+        infinite: true,
+        dots: true,
+        autoplay: true,
+        speed: 300,
+        slidesToShow: 1,
+        //adaptiveHeight: true,
+        arrows : 'none'
+    } );
 }
 
 function shad() {
@@ -21,4 +49,8 @@ function shad() {
 $( document ).ready( function () {
    bigSlider();
    shad();
+   carousel();
+    initMap();
+    submitForm();
 });
+
