6ea1e5dc74f869b3cc33ab065a8fdddb56f94a34
rostic54 <rostic54@gmail.com>
2017-07-16 19:28:54 +0300

    finish

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 function initMap() {
 
-    var map = new google.maps.Map(document.getElementById('map'), {
+    let map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 8.9194172, lng: 76.6418569},
         scrollwheel: false,
         zoom: 14
@@ -10,47 +10,100 @@ function initMap() {
 }
 
 function submitForm() {
-    $( '#check' ).click( function () {
-         $( '.js-script-form' ).submit();
+    $('#check').click(function () {
+        $('.js-script-form').submit();
     });
 }
 
 function bigSlider() {
-    $( '.js-script-slider' ).slick( {
-        infinite:true,
-        arrows: true,
+    $('.js-script-slider').slick({
+        infinite: true,
+        arrows: true
         //fade: true
 
-    } );
+    });
 
 }
 
 function carousel() {
-    $( '.js-script-carousel' ).slick( {
+    $('.js-script-carousel').slick({
         infinite: true,
         dots: true,
         autoplay: true,
         speed: 300,
         slidesToShow: 1,
-        //adaptiveHeight: true,
-        arrows : 'none'
-    } );
+        arrows: 'none'
+    });
 }
 
 function shad() {
-    $( '.js-script-shadow' ).flatshadow( {
+    $('.js-script-shadow').flatshadow({
         color: "#61e199",
         fade: true,
         angle: "SW",
         boxShadow: "transparent"
-    } )
+    })
 }
 
-$( document ).ready( function () {
-   bigSlider();
-   shad();
-   carousel();
+function validForm() {
+    let form = $('form');
+    for (let i = 0; i < form.length; i++) {
+        $(form[i]).validate({
+            rules:{
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                email:{
+                    required: true,
+                    email: true
+                },
+                zip: {
+                    required: true,
+                    number: true
+                }
+            },
+            focusCleanup: true,
+            focusInvalid: false,
+            onfocusout: false,
+            massages: false,
+            errorPlacement: function (error, element) {
+
+                let id = element.parent().attr( 'id' );
+
+                if( id === 'circle' ) {
+                    $('.js-script-errorBlock').text('NOT CORRECT');
+                    element.parent().css('margin-top', '0px');
+                }else{
+                    $( '.js-script-invalid' ).removeClass( "is-hidden" );
+                    $( '.js-script-valid' ).addClass( 'is-hidden' );
+
+                }
+            },
+            onkeyup: function (element, event) {
+                //let target = element.attr( 'class' );
+
+                if( $( element ).hasClass( 'js-script-active' ) ) {
+                    $(".js-script-errorBlock").text('');
+                    $( element ).parent().css('margin-top', '');
+                }else{
+                    $( '.js-script-valid' ).removeClass( "is-hidden" );
+                    $( '.js-script-invalid' ).addClass( 'is-hidden' );
+                }
+
+            }
+
+
+        });
+    }
+}
+
+$(document).ready(function () {
+    bigSlider();
+    shad();
+    carousel();
     initMap();
     submitForm();
+    validForm();
 });
 
